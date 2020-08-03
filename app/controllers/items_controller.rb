e2e70d7f02ecd966eec1d77611b72b7a6fd30cf3
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-03 11:50:23 +0900

    ログインしていないと出品ページにいけないようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :logged_in_user?, only: [:new, :create]
   def index
   end
 
@@ -8,4 +9,9 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+    def logged_in_user?
+      return redirect_to new_user_session_path unless user_signed_in?
+    end
 end
