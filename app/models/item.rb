87fbd7ac9cedf36280574a0d2c72c232c148f415
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-03 11:38:44 +0900

    ActiveHashで使用するモデルの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+	extend ActiveHash::Associations::ActiveRecordExtensions
+	belongs_to_active_hash :genre
+	belongs_to_active_hash :status
+	belongs_to_active_hash :delivery_fee
+	belongs_to_active_hash :prefecture
+	belongs_to_active_hash :shipment
 	belongs_to :user
 	has_one_attached :image
 
