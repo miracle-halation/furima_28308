8d80b9814c823e96a5c1bde4c61cefb52031aa8f
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-03 11:55:43 +0900

    createアクションとストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,28 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new()
+    @item = Item.new
   end
 
   def create
+    item = item.new(item_params)
+    if item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
-    def logged_in_user?
-      return redirect_to new_user_session_path unless user_signed_in?
-    end
+
+  def item_params
+    params.require(:item)
+          .permit(:name, :price, :genre_id, :status_id,
+                  :delivery_fee_id, :prefecture_id, :shipment_id)
+          .merge(user_id: current_user.id)
+  end
+
+  def logged_in_user?
+    return redirect_to new_user_session_path unless user_signed_in?
+  end
 end
