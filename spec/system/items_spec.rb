492e17a3b55c510792d7648d095a26f7d23ec776
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-05 11:13:46 +0900

    購入済みであるならトップページへ遷移する結合テストを実装

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -110,6 +110,11 @@ RSpec.describe 'Items', type: :system do
         expect(current_path).to eq "/items/#{item.id}"
         expect(page).to have_content "Name can't be blank"
       end
+      it "すでに購入済みであるなら、トップページへ遷移する" do
+        sign_in(order.item.user)
+        visit edit_item_path(order.item)
+        expect(current_path).to eq root_path
+      end
     end
 
     context '編集に成功した時' do
