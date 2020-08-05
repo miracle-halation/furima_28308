6efbec7f8f292d6531e3bb3c2b81acb978eadbd1
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-05 17:17:06 +0900

    transactionsのindexのアクセス制限

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,31 @@
 class TransactionsController < ApplicationController
+	before_action :logged_in_user?, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :item_user?, only: [:index, :create]
+	before_action :sold_out?, only: [:index, :create]
+	
 	def index
 	end
 
 	def create
 	end
+
+	private
+
+
+  def logged_in_user?
+    return redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def item_user?
+    return redirect_to root_path if current_user == @item.user
+  end
+
+  def sold_out?
+    return redirect_to root_path unless @item.order.nil?
+  end
 end
