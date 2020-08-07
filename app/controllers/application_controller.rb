50f7706ab3504b9060effe86a3c132ed1e6c2c50
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-07 15:03:37 +0900

    デバッグ終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :basic_auth
+  before_action :basic_auth
 
   private
 
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
                                   [:nickname, :family_name, :family_name_reading, :first_name, :first_name_reading, :birthday])
   end
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-  #   end
-  # end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
